96c6ea0e8f864bd6aebeeb55ca52a5c985e0e33c
shahnewaz601 <shahnewaz601@gmail.com>
2021-10-31 02:40:49 +0600

    designed header bottom banner

--- src/components/Home/CategoriesSlider/CategoriesSlider.js
+++ src/components/Home/CategoriesSlider/CategoriesSlider.js
@@ -1,32 +1,70 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Row, Col, Image } from 'react-bootstrap';
+import { IoArrowForwardCircle } from "react-icons/io5";
 import localImages from '../../shared/localImages';
 import './CategoriesSlider.css';
 
 const CategoriesSlider = () => {
 
     return (
-        <section className="mt-3 mb-5 categories-slider">
+        <section className="mt-4 mb-5 categories-slider">
             <Container className="custom-container">
                 <Row>
-                    <Col lg={3}>
-                        <div className="cBanner-img">
-                            <Image src={localImages.pearBanner} fluid />
-                        </div>
-                    </Col>
-                    <Col lg={3}>
-                        <div className="cBanner-img">
+                    <Col lg={3} md={6} className="col-6 mb-md-3" >
+                        <div className="hdr-banner">
                             <Image src={localImages.pomegranateBanner} fluid />
+                            <div className="hdr-description">
+                                <p className="text-uppercase m-0">100% natural</p>
+                                <h2>Pomegranate</h2>
+                                <Link to="/a">
+                                    <span className="text-uppercase">Shop now</span>
+                                    <span><IoArrowForwardCircle /></span>
+                                </Link>
+                            </div>
+                            <div className="overlay" />
                         </div>
                     </Col>
-                    <Col lg={3}>
-                        <div className="cBanner-img">
+                    <Col lg={3} md={6} className="col-6 mb-md-3" >
+                        <div className="hdr-banner">
+                            <Image src={localImages.pearBanner} fluid />
+                            <div className="hdr-description">
+                                <p className="text-uppercase m-0">100% natural</p>
+                                <h2>Pear</h2>
+                                <Link to="/a">
+                                    <span className="text-uppercase">Shop now</span>
+                                    <span><IoArrowForwardCircle /></span>
+                                </Link>
+                            </div>
+                            <div className="overlay" />
+                        </div>
+                    </Col>
+                    <Col lg={3} md={6} className="col-6 mb-md-3" >
+                        <div className="hdr-banner">
                             <Image src={localImages.fruitBanner} fluid />
+                            <div className="hdr-description">
+                                <p className="text-uppercase m-0">100% natural</p>
+                                <h2>Fruits</h2>
+                                <Link to="/a">
+                                    <span className="text-uppercase">Shop now</span>
+                                    <span><IoArrowForwardCircle /></span>
+                                </Link>
+                            </div>
+                            <div className="overlay" />
                         </div>
                     </Col>
-                    <Col lg={3}>
-                        <div className="cBanner-img">
+                    <Col lg={3} md={6} className="col-6 mb-md-3" >
+                        <div className="hdr-banner">
                             <Image src={localImages.appleBanner} fluid />
+                            <div className="hdr-description">
+                                <p className="text-uppercase m-0">100% natural</p>
+                                <h2>Apple</h2>
+                                <Link to="/a">
+                                    <span className="text-uppercase">Shop now</span>
+                                    <span><IoArrowForwardCircle /></span>
+                                </Link>
+                            </div>
+                            <div className="overlay" />
                         </div>
                     </Col>
                 </Row>
--- src/components/shared/localImages.js
+++ src/components/shared/localImages.js
@@ -2,7 +2,7 @@ const localImages = {
    bannerImg1: require('../../images/banner/banner1.jpg').default,
    bannerImg2: require('../../images/banner/banner2.jpg').default,
    bannerImg3: require('../../images/banner/banner3.jpg').default,
-   fruitBanner: require('../../images/banner/banner6.jpg').default,
+   fruitBanner: require('../../images/banner/banner10.png').default,
    pomegranateBanner: require('../../images/banner/banner9.jpg').default,
    pearBanner: require('../../images/banner/banner7.jpg').default,
    appleBanner: require('../../images/banner/banner8.jpg').default
